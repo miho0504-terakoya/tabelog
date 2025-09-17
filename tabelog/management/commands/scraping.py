5151a2bd3108a33c251a6748f91308dc238708ad
miho0504-terakoya <miho.okt54@gmail.com>
2025-09-17 17:08:34 +0900

    修正

--- tabelog/management/commands/scraping.py
+++ tabelog/management/commands/scraping.py
@@ -3,17 +3,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-#from getpass import getpass
-#from bs4 import BeautifulSoup
 import time
 import re
 from tabelog.models import Restaurant
-from datetime import datetime #日付上限を設定して新規店舗を抽出
+from datetime import datetime, timedelta #日付上限を設定して新規店舗を抽出
 
 class Command(BaseCommand):
     help = "食べログスクレイピング処理"
-    
-    def handle(self, *args, **options):
+
+
+    def get_new_shop_data(self, station_name):
         print("スクレイピング処理")
 
         # ヘッドレスモードで起動するためのオプションを設定
@@ -32,8 +31,9 @@ class Command(BaseCommand):
         # 最大30秒間、トップページが表示されるのを待つ
         wait = WebDriverWait(chrome_driver, 30)
 
-        # エリアを指定して入力する　※同じ駅名の場合どうすればよいか
-        station_name = input('駅名を入力してください：')
+        
+        # エリアを指定して入力する
+        # station_name = input('駅名を入力してください：')
         pernet_element = chrome_driver.find_element(By.CSS_SELECTOR, '#react-search-header > form > div.sc-fHSyak.sc-dubCtV.ljbUvS.hhzuBp')
         station_input = pernet_element.find_element(By.CLASS_NAME, 'sc-uhnfH.hGDSLA')
         station_input.send_keys(station_name)
@@ -56,12 +56,13 @@ class Command(BaseCommand):
         time.sleep(10)
         chrome_driver.save_screenshot('screenshot.png')
         
-        # 店舗リストを取得（上限日を指定）
-        restaurants = chrome_driver.find_elements(By.CSS_SELECTOR, 'div.list-rst__contents')
-        limit_date = datetime(2025, 9, 1)
-        
+
         while True:
         
+            # 店舗リストを取得（上限日を指定）
+            restaurants = chrome_driver.find_elements(By.CSS_SELECTOR, 'div.list-rst__contents')
+            limit_date = datetime.now() - timedelta(days=30)
+
             for restaurant in restaurants:
                 try:
                     name = restaurant.find_element(By.CSS_SELECTOR, 'a.list-rst__rst-name-target').text
@@ -85,8 +86,8 @@ class Command(BaseCommand):
                                 "name": name,
                                 "url": url,
                                 "open_date": open_date
-                            }
-                        )
+                                }
+                             )
                         print(f"保存: {station_name} -{name} ({restaurant_id} {url} {open_date})")
                     else:
                         print(f"スキップ: {name} - {open_date}")
@@ -95,16 +96,20 @@ class Command(BaseCommand):
                 except Exception as e:
                     print(f"保存失敗: {e}")
             else:
-                # 次のページボタンを押下
-                page_no += 1
+                # 次のページボタンを押
+                next_button = chrome_driver.find_element(By.CSS_SELECTOR, 'a.c-pagination__arrow--next')
+                next_button.click()
+                time.sleep(10)
                 continue
             
             break   #  whileループを抜ける(途中でスキップした場合)
-
-                
         chrome_driver.quit()
         print("スクレイピング処理終了")
-        
-        #ニューオープンが指定日付だけでなければ、次のページへ遷移する
-        #ニューオープンの日付を今日の日付から30日前移行の日付にする
-        #次のページに押下するのところで、何ページ目のボタンを押下するかを記述する、ページナンバー
+
+    def handle(self, *args, **options):
+        stations = [
+            "東京駅", "品川駅"
+        ]
+        # stations = データベースのマスター情報から取得する
+        for station in stations:
+            self.get_new_shop_data(station)
