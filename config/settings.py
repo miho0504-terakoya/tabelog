b4b58a7034252d102a3c6aff0eb3bccd65180549
miho0504-terakoya <miho.okt54@gmail.com>
2025-10-07 21:57:54 +0900

    デバック追加

--- config/settings.py
+++ config/settings.py
@@ -117,8 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"] 
- 
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+else:
+    STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
